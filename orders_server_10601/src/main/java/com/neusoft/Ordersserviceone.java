b22972114392765a5f39ba31704f1d5fca972800
lisoner <156236466+lisoner@users.noreply.github.com>
2024-07-05 20:11:38 +0800

    Add files via upload

---
+++ orders_server_10601/src/main/java/com/neusoft/Ordersserviceone.java
@@ -0,0 +1,11 @@
+package com.neusoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ordersserviceone {
+    public static void main(String[] args) {
+        SpringApplication.run(Ordersserviceone.class,args);
+    }
+}
